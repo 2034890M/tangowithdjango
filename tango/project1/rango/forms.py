599fe59848c07c41a41e0dbbdb1cb6621e21068f
2034890M <2034890M@ip-10-155-13-24.ec2.internal>
2015-03-13 18:25:28 +0000

    Fixed redirecting errors

--- tango/project1/rango/forms.py
+++ tango/project1/rango/forms.py
@@ -30,7 +30,7 @@ class PageForm(forms.ModelForm):
         # we can either exclude the category field from the form,
         exclude = ('category',)
         #or specify the fields to include (i.e. not include the category field)
-        #fields = ('title', 'url', 'views')
+        fields = ('title', 'url', 'views')
 
     def clean(self):
         cleaned_data = self.cleaned_data
